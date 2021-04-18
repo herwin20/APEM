b0ee5acd29e602b36378b44db6548f1b774070ba
herwin20 <herwinjanuardi20@gmail.com>
2021-04-19 06:24:49 +0700

    file untuk main activity baru

--- app/src/main/java/com/herwinlab/apem/testprogram/TesNotifikasi.java
+++ app/src/main/java/com/herwinlab/apem/testprogram/TesNotifikasi.java
@@ -2,9 +2,12 @@ package com.herwinlab.apem.testprogram;
 
 import android.app.Activity;
 import android.app.Notification;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import androidx.annotation.RequiresApi;
@@ -23,9 +26,11 @@ public class TesNotifikasi extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.notifikasi_activity);
+        setContentView(R.layout.new_main_activity);
 
-        mNotificationUtils = new NotificationUtils(this);
+        transparentStatusAndNavigation();
+
+      /*  mNotificationUtils = new NotificationUtils(this);
         tesNotif = findViewById(R.id.btnShowNotif);
 
         tesNotif.setOnClickListener(new View.OnClickListener() {
@@ -34,7 +39,38 @@ public class TesNotifikasi extends AppCompatActivity {
                 Notification.Builder nb = mNotificationUtils.getAndroidChannelNotification("Tes Notifikasi","Herwin Ganteng");
                 mNotificationUtils.getManager().notify(101, nb.build());
             }
-        });
+        });*/
     }
 
+    private void transparentStatusAndNavigation() {
+        //make full transparent statusBar
+        if (Build.VERSION.SDK_INT >= 19 && Build.VERSION.SDK_INT < 21) {
+            setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION, true);
+        }
+        if (Build.VERSION.SDK_INT >= 19) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            );
+        }
+        if (Build.VERSION.SDK_INT >= 21) {
+            setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION, false);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+            getWindow().setNavigationBarColor(Color.TRANSPARENT);
+        }
+    }
+
+    private void setWindowFlag(final int bits, boolean on) {
+        Window win = getWindow();
+        WindowManager.LayoutParams winParams = win.getAttributes();
+        if (on) {
+            winParams.flags |= bits;
+        } else {
+            winParams.flags &= ~bits;
+        }
+        win.setAttributes(winParams);
+    }
 }
